1f8529ff54e8e378f9fc7ed381152f002df78a25
zachey <63107653+zachey01@users.noreply.github.com>
2024-07-09 18:44:37 +0500

    Update index.cjs

--- index.cjs
+++ index.cjs
@@ -1,5 +1,8 @@
 "use strict";
 module.exports = async function () {
   const { default: GPT4js } = await import("./index.js");
+  if (typeof window !== "undefined") {
+    window.GPT4js = GPT4js;
+  }
   return GPT4js;
 };
