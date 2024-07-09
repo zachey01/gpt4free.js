b28fb16970323121b9a0783def0e2fdb3d3c9b77
zachey <63107653+zachey01@users.noreply.github.com>
2024-07-09 14:33:06 +0500

    Delete test.cjs

--- test.cjs
+++
@@ -1,7 +0,0 @@
-const getGPT4js = require("./index.cjs");
-
-(async () => {
-  const GPT4js = await getGPT4js();
-  const provider = GPT4js.createProvider("Aryahcr");
-  console.log(provider);
-})();
