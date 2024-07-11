8f6e23d00427908d048574e9b049f1e3d8d9a3b4
zachey <63107653+zachey01@users.noreply.github.com>
2024-07-11 14:23:35 +0500

    Remove red cross

--- index.cjs
+++ index.cjs
@@ -1,7 +1,6 @@
 "use strict";
 module.exports = async function () {
   const { default: GPT4js } = await import("./index.js");
-  require("./src/Utils/redCross.js");
   if (typeof window !== "undefined") {
     window.GPT4js = GPT4js;
   }
--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import GPT4js from "./src/providers.js";
-import "./src/Utils/redCross.js";
 
 if (typeof window !== "undefined") {
   window.GPT4js = GPT4js;
