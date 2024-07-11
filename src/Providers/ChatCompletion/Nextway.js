98008bc8ce8366a6a6cfce9a318b0c977962b982
zachey <63107653+zachey01@users.noreply.github.com>
2024-07-12 01:22:19 +0500

    Update Nextway.js

--- src/Providers/ChatCompletion/Nextway.js
+++ src/Providers/ChatCompletion/Nextway.js
@@ -3,30 +3,32 @@ import Provider from "./provider.js";
 
 class NextwayProvider extends Provider {
   async chatCompletion(messages, options) {
-    const response = await fetch(
-      "https://chat.eqing.tech/api/openai/v1/chat/completions",
-      {
-        headers: {
-          accept: "text/event-stream",
-          "content-type": "application/json",
-          usesearch: "false",
-        },
-        body: JSON.stringify({
-          messages: messages,
-          stream: options.stream,
-          model: options.model || "gpt-4o-free",
-          temperature: options.temperature,
-          captchaToken:
-            "P1_" +
-            btoa(JSON.stringify({ typ: "JWT", alg: "HS256" })) +
-            "." +
-            btoa("hashedkey") +
-            "." +
-            btoa("signature"),
-        }),
-        method: "POST",
-      }
-    );
+    const proxyUrl = options.use_proxy
+      ? "https://proxy.zachey.space/?url=https://chat.eqing.tech/api/openai/v1/chat/completions"
+      : "https://chat.eqing.tech/api/openai/v1/chat/completions";
+
+    const response = await fetch(proxyUrl, {
+      headers: {
+        accept: "text/event-stream",
+        "content-type": "application/json",
+        usesearch: "false",
+      },
+      body: JSON.stringify({
+        messages: messages,
+        stream: options.stream,
+        model: options.model || "gpt-4o-free",
+        temperature: options.temperature,
+        captchaToken:
+          "P1_" +
+          btoa(JSON.stringify({ typ: "JWT", alg: "HS256" })) +
+          "." +
+          btoa("hashedkey") +
+          "." +
+          btoa("signature"),
+      }),
+      method: "POST",
+    });
+
     if (!response.ok) {
       console.error("Network response was not ok");
       return;
