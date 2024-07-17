d221ccb6f11539782355b0d6a2c9c7752fda8194
zachey <63107653+zachey01@users.noreply.github.com>
2024-07-18 03:29:03 +0500

    Update Nextway.js

--- src/Providers/ChatCompletion/Nextway.js
+++ src/Providers/ChatCompletion/Nextway.js
@@ -6,25 +6,20 @@ class NextwayProvider extends Provider {
   async chatCompletion(messages, options) {
     const proxyUrl = options.use_proxy
       ? "https://proxy.zachey.space/?url=https://chat.eqing.tech/api/openai/v1/chat/completions"
-      : "https://chat.eqing.tech/api/openai/v1/chat/completions";
+      : "https://origin.eqing.tech/api/openai/v1/chat/completions";
 
     const response = await fetch(proxyUrl, {
       headers: {
-        ...baseHeaders("https://chat.eqing.tech"),
-        usesearch: options.webSearch,
+        ...baseHeaders("https://origin.eqing.tech/"),
+        usesearch: "true",
       },
       body: JSON.stringify({
         messages: messages,
         stream: options.stream,
         model: options.model || "gpt-4o-free",
-        temperature: options.temperature,
-        captchaToken:
-          "P1_" +
-          btoa(JSON.stringify({ typ: "JWT", alg: "HS256" })) +
-          "." +
-          btoa("hashedkey") +
-          "." +
-          btoa("signature"),
+        temperature: options.temperature || 0.5,
+        max_tokens: 4000 || options.maxTokens,
+        captchaToken: `P1_${[...Array(30)].map(() => "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_".charAt(Math.floor(Math.random() * 64))).join("")}.${[...Array(256)].map(() => "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_".charAt(Math.floor(Math.random() * 64))).join("")}.${[...Array(43)].map(() => "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_".charAt(Math.floor(Math.random() * 64))).join("")}`,
       }),
       method: "POST",
     });
