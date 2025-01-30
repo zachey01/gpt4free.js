42e5a47d876f7e8a39c058985db9487966d3b6b8
Zachey <63107653+zachey01@users.noreply.github.com>
2025-01-30 16:23:19 +0500

    Fix

--- src/index.js
+++ src/index.js
@@ -1,23 +1,9 @@
-// src/index.js
-import AryahcrProvider from "./providers/chatCompletion/Aryahcr.js";
-import BlackBoxProvider from "./providers/chatCompletion/BlackBox.js";
-import NextwayProvider from "./providers/chatCompletion/Nextway.js";
 import ChromeProvider from "./providers/chatCompletion/Chrome.js";
 import OllamaProvider from "./providers/chatCompletion/Ollama.js";
-import AlibabaProvider from "./providers/chatCompletion/AliBaba.js";
-
-import DALLEProvider from "./providers/imageGeneration/DALLE.js";
-import StableDiffusionProvider from "./providers/imageGeneration/StableDiffusion.js";
 
 const PROVIDERS = {
-  Aryahcr: AryahcrProvider,
-  BlackBox: BlackBoxProvider,
-  Nextway: NextwayProvider,
   Chrome: ChromeProvider,
   Ollama: OllamaProvider,
-  Alibaba: AlibabaProvider,
-  DALLE: DALLEProvider,
-  StableDiffusion: StableDiffusionProvider,
 };
 
 class GPT4js {
